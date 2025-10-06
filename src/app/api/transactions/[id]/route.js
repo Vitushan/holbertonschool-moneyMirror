08597c68b845542ac6812bd9c9060711fc0b293a
Vitushan <vitushan@outlook.fr>
2025-10-06 14:17:20 +0200

    create, get and get for id is finish

--- src/app/api/transactions/[id]/route.js
+++ src/app/api/transactions/[id]/route.js
@@ -1,13 +1,13 @@
-import { prisma } from "../../../../lib/prisma"
-import { getServerSession } from "next-auth"
-import { authOptions } from "../auth/[...nextauth]/route"
-import { NextResponse } from "next/server"
+import { prisma } from "../../../../lib/prisma" //Import the Prisma Client instance configured for the MySQL DB
+import { getServerSession } from "next-auth" //Imports the NextAuth function which allows you to retrieve the session on the server side (check if the user is logged in)
+import { authOptions } from "../auth/[...nextauth]/route" // Imports the NextAuth configuration (providers, callbacks, etc.). getServerSession(authOptions) needs these options to validate the session
+import { NextResponse } from "next/server" //Next.js utility to construct and return HTTP responses from a route handler (App Router) used to return JSON with a status.
 
-
+//this is a findone for my crud
 // this route is used to read a single specific transaction from its unique ID.
-export async function GET(request, { params }) {
+export async function GET(request, { params }) { //{ params } = Next.js injects the dynamic parameters of the URL here (e.g. for /api/transactions/[id], params.id contains the ID).
   try {
-    const session = await getServerSession(authOptions)
+    const session = await getServerSession(authOptions) //Retrieves the user session (if the user is logged in)
     if (!session || !session.user) {
       return NextResponse.json({ error: "User not authenticated" }, { status: 401 });
     }
@@ -16,10 +16,10 @@ export async function GET(request, { params }) {
       return NextResponse.json({ error: "Invalid transaction ID" }, {status: 400 })
     }
     //findFirst allows you to add multiple conditions (id + userId).
-    const transaction = await prisma.transaction.findFirst({ // Searches for the matching transaction belonging to the user
+    const transaction = await prisma.transaction.findFirst({ // Searches for the matching transaction belonging to the user (Prisma Query)
       where: {
-        id: id,
-        userId: session.user.id,
+        id: id, //search for the requested ID
+        userId: session.user.id, // we check that the transaction belongs to the connected user (security/ownership).
       },
     });
 
@@ -29,7 +29,7 @@ export async function GET(request, { params }) {
     return NextResponse.json({ success: true, transaction }, { status: 200});
 
   } catch (error) {
-    console.log("Error fetching transaction", error)
+    console.error("Error fetching transaction", error)
     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
   }
 }
