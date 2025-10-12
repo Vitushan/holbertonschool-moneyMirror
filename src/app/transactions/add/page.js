82dcda05f8c57698836fe99d028fa0df925093a9
Vitushan <vitushan@outlook.fr>
2025-10-12 15:27:57 +0200

    connected API form for Frond ENd

--- src/app/transactions/add/page.js
+++ src/app/transactions/add/page.js
@@ -1,10 +1,59 @@
-//form add transactions
+// Add Transaction form (API connected)
+
+
+import { Currency } from "lucide-react";
+import { useState } from "react"
+
 
 export default function AddTransactionPage() {
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setMessage("");
+        setError("");
+        setLoading(true);
+
+
+        const form = e.target;
+        const data = {
+            amount: form.amount.value,
+            type: form.type.value,
+            category: form.category.value,
+            description: form.description.value,
+            note: form.note.value,
+            currency: form.currency.value,
+            date: form.date.value,
+        };
+
+        try {
+            const res = await fetch("/api/transactions", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data),
+        });
+        if (!res.ok) {
+            const err = await res.json();
+            setError(err.message || "Add Error")
+        } else {
+            setMessage("Transaction successfully added!");
+            form.reset();
+        }
+} catch (err) {
+    setError("Network or server error")
+} finally {
+    setLoading(false);
+}
+}
+
     return (
         <div>
-            <h1>Add Transaction</h1>
-            <form>
+            <h1>
+                <Currency size={24} color="#007bff" style={{ verticalAlign: "middle"}} /> Add Transaction
+            </h1>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label>Amount:</label>
                     <input type="number" name="amount" required />
@@ -37,8 +86,12 @@ export default function AddTransactionPage() {
                         <label>Date:</label>
                         <input type="date" name="date" required />
                 </div>
-                <button type="submit">Add</button>
+                <button type="submit" disabled={loading}>
+                    {loading ? "Adding in progress...": "Add"}
+                </button>
             </form>
+            {message && <p style={{ color: "green" }}>{message}</p>}
+            {error && <p style={{ color: "red" }}>{error}</p>}
         </div>
-     )
+     );
 }
