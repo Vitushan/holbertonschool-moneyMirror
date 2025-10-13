f07ff0abc0489f8c68f53a38b18deb5508a6e2bb
Vitushan <vitushan@outlook.fr>
2025-10-13 22:55:22 +0200

    working conditional rendering for display transactions lists

--- src/app/transactions/page.js
+++ src/app/transactions/page.js
@@ -4,8 +4,31 @@
 import { useEffect, useState } from "react" //manage state and side effects (fetch API, etc.) [useEffect for start fetch)]
 
 
-export default function TransactionsPage() { //transactions lists
+export default function TransactionsPage() { // display transactions lists
     const [transactions, setTransactions] = useState([]) // will contain the list of transactions retrieved from the API
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(true) // manage the display of the loader
+    const [error, setError] = useState('') // manage the display of the loader
+
+    useEffect(() => {
+        async function fetchTransactions() { // noted loading start
+            setLoading(true);
+            setError('');
+            try {
+                const res = await fetch('/api/transactions'); // CALL API
+                if (!res.ok) throw new Error('Error while fetching');
+                const data = await res.json(); // get json data
+                setTransactions(data); // stores transactions in the state and automatically updates the UI
+            } catch (err) {
+                setError(err.message || 'Unknown error');  // stores the error if needed
+            } finally {
+                setLoading(false); // loading is finished
+            }
+        }
+        fetchTransactions(); //launch the function
+    }, []); // if [] is empty = only on page load
+
+   // conditional rendering
+   if (loading) {
+    return 
+   }
 }
