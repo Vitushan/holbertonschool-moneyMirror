26f881083cc89f092b835da56f6b45c984af94ef
Vitushan <vitushan@outlook.fr>
2025-10-20 15:28:06 +0200

    preparing dashboard task

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+// Dashboard page, Main user interface
+// Displays statistics cards, charts, and filters
+// Protected route, requires authentication
+
+'use client'
+
+import { useSession } from 'next-auth/react' // Import the useSession hook to manage user authentication state
+import { useRouter } from 'next/navigation' // Import the useRouter hook to programmatically navigate between pages
+import { useState, useEffect } from 'react' // Import React hooks for managing component state and lifecycle
+
+export default function DashboardPage() {
+    
+}
