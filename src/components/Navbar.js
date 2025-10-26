dc69b06c12e12e97041679d3f81c4066c12455f7
Vitushan <vitushan@outlook.fr>
2025-10-26 12:33:35 +0100

    resole piggy banks problems

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
     { name: 'Tableau de bord', href: '/dashboard' },
     { name: 'Transactions', href: '/transactions' },
     { name: 'Ajouter Transaction', href: '/transactions/add' },
-    { name: 'Tirelires 🐷', href: '/piggybanks' },
+    { name: 'Piggy Bank 🐷', href: '/piggybanks' },
   ]
 
   const supportButton = { name: 'Soutenir 💙', href: '/donation' }
